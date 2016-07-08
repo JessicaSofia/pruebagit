18b559f0a3794be8e8c0446775e6938761898370
Jessi <Jessi@Jessy>
2016-07-08 16:58:53 -0500

    inicial

---
+++ demo_prueba/src/main/java/com/example/DemoPruebaApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPruebaApplication.class, args);
+	}
+}
